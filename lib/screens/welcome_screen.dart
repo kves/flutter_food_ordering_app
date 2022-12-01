e3998f9bf02eb5919f1cd531e560568b9b84b7c1
Kves <vnlkbn@gmail.com>
2022-12-02 00:03:45 +0100

    welcome zreen done

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,144 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_food_ordering_app/theme/colors.dart';
+
+import '../pages/home_page.dart';
+import '../theme/colors.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: CustomColors.customPrimaryColor,
+      body: Column(
+        children: [
+          // burger image
+
+          Stack(
+            children: [
+              Opacity(
+                opacity: 0.3,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 90,
+                    right: 90,
+                    top: 30,
+                    bottom: 20,
+                  ),
+                  child: Image.asset(
+                      'lib/resources/images/products/burger_welcome.png',
+                      color: Colors.white),
+                ),
+              ),
+              ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 90,
+                      right: 90,
+                      top: 30,
+                      bottom: 20,
+                    ),
+                    child: Image.asset(
+                        'lib/resources/images/products/burger_welcome.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          //texts container
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(46),
+                    bottomRight: Radius.circular(46)),
+                color: CustomColors.customPrimaryColor,
+                border: Border.all(
+                  color: Colors.grey.withOpacity(0.3),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 50,
+                  ),
+                ]),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+            child: Column(
+              children: [
+                const Text(
+                  'Welcome',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'TO',
+                  style: TextStyle(
+                      color: CustomColors.customOrange,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'NH Food Delivery Shop',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+
+                //let's go button
+                GestureDetector(
+                  onTap: () => Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: ((context) {
+                    return const HomePage();
+                  }))),
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      left: 120,
+                      right: 120,
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: CustomColors.customOrange,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(46),
+                          bottomRight: Radius.circular(46)),
+                    ),
+                    child: const Text(
+                      "Let's Go",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/theme/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const customPrimaryColor = Color(0xFF001314);
+  static const customOrange = Color(0xFFff8e24);
+}
+
+const MaterialColor customOrange = MaterialColor(0xFFff8e24, <int, Color>{
+  50: Color(0xFFff8e24),
+  100: Color(0xFFff8e24),
+  200: Color(0xFFff8e24),
+  300: Color(0xFFff8e24),
+  400: Color(0xFFff8e24),
+  500: Color(0xFFff8e24),
+  600: Color(0xFFff8e24),
+  700: Color(0xFFff8e24),
+  800: Color(0xFFff8e24),
+  900: Color(0xFFff8e24),
+});
